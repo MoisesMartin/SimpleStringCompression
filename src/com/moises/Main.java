8e5eaf670a17f1a37dc1155afa89dbb76ed448c3
Moises Martin <moises01@live.com.mx>
2021-05-11 22:01:06 -0500

    Commit 3 :p
    Signed-off-by: Moises Martin <moises01@live.com.mx>

--- src/com/moises/Main.java
+++ src/com/moises/Main.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Loading user input JEJE
         String input="";
         System.out.println("Escribe una palabra: ");
         Scanner userInput = new Scanner(System.in);
-
+        input = userInput.next();
+        StringCompressor compressor = new StringCompressor();
+        //ejecutando el metodo sortCharacters el cual necesita del metodo splitWord
+        compressor.sortCharacters(compressor.splitWord(input));
+        compressor.showList();
     }
 }
--- src/com/moises/StringCompressor.java
+++ src/com/moises/StringCompressor.java
@@ -1,4 +1,34 @@
 package com.moises;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class StringCompressor {
+    private ArrayList<Character> chars = new ArrayList<>();
+
+    //Default Constructor
+    public StringCompressor(){
+
+    }
+    //
+
+    public ArrayList<Character> splitWord(String userInput){
+        for(int i = 0; i<userInput.length();i++){
+            chars.add(userInput.charAt(i));
+        }
+        return chars;
+    }
+    public ArrayList<Character> sortCharacters(ArrayList<Character> chars){
+        Collections.sort(chars);
+        return chars;
+    }
+    //If invoked before sorting it will just show the char list as given originally
+    public void showList(){
+        for(int i=0; i<chars.size();i++){
+            System.out.print(chars.get(i) +", ");
+            if(i == chars.size()-1){
+                System.out.print(chars.get(i));
+            }
+        }
+    }
 }
